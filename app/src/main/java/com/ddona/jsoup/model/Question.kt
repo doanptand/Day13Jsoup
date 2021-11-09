b035ddff257e6b38407a928f58dc9c16868bb7bd
DoanPT1 <doanptand@gmail.com>
2021-11-09 19:38:50 +0700

    use jsoup to extract data

---
+++ app/src/main/java/com/ddona/jsoup/model/Question.kt
@@ -0,0 +1,10 @@
+package com.ddona.jsoup.model
+
+data class Question(
+    var title: String = "",
+    var description: String = "",
+    var vote: String = "",
+    var date: String = "",
+    var author: String = "",
+    var link: String = "",
+)
---
+++ app/src/main/java/com/ddona/jsoup/parser/StackOverFlowParser.kt
@@ -0,0 +1,38 @@
+package com.ddona.jsoup.parser
+
+import com.ddona.jsoup.model.Question
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+
+object StackOverFlowParser {
+
+    fun parserQuestion(question: String): List<Question> {
+        val questions = arrayListOf<Question>()
+        val link = "https://stackoverflow.com/search?q=$question"
+        val document: Document = Jsoup.connect(link).timeout(10000).get()
+
+        val questionElements = document.getElementsByClass("question-summary search-result")
+
+        for (i in 0 until questionElements.size) {
+            val questionElement: Element = questionElements[i]
+            val voteElement: Element =
+                questionElement.getElementsByClass("status answered-accepted")[0]
+            val titleElement = questionElement.getElementsByClass("question-hyperlink")[0]
+            val descriptionElement = questionElement.getElementsByClass("excerpt")[0]
+            val timeElement = questionElement.getElementsByClass("relativetime")[0]
+            val authorElement = questionElement
+                .getElementsByClass("started fr")[0]
+                .getElementsByTag("a")[0]
+            val vote = voteElement.text()
+            val title = titleElement.text()
+            val description = descriptionElement.text()
+            val time = timeElement.text()
+            val url = titleElement.attr("href")
+            val author = authorElement.text()
+            questions.add(Question(title, description, vote, time, author, url))
+        }
+        return questions
+    }
+}

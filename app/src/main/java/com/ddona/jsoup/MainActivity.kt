7f83e696c2dc13a46f674a4ab2784cdb9e0b8940
DoanPT1 <doanptand@gmail.com>
2021-11-09 20:36:02 +0700

    use coroutine to run

--- app/src/main/java/com/ddona/jsoup/MainActivity.kt
+++ app/src/main/java/com/ddona/jsoup/MainActivity.kt
@@ -2,7 +2,12 @@ package com.ddona.jsoup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.ddona.jsoup.databinding.ActivityMainBinding
+import com.ddona.jsoup.parser.StackOverFlowParser
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -10,5 +15,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        lifecycleScope.launch(Dispatchers.IO) {
+            val question = StackOverFlowParser.parserQuestion("Jetpack")
+            Log.d("doanpt", "Get question size is ${question.size}")
+        }
     }
 }
